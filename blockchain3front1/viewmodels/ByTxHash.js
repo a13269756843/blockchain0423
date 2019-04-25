825e78b58d991f4b2280293e8622b0977c29f839
a13269756843 <984171281@qq.com>
2019-04-25 10:09:00 +0800

    add front selecttransaction by Txhash

---
+++ blockchain3front1/viewmodels/ByTxHash.js
@@ -0,0 +1,28 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      showTx:[],
+      txhash:''
+    },
+    mounted(){
+      var url = new URL(location.href);
+      var txhash = url.searchParams.get('txhash');
+      this.getTx(txhash);
+    },
+    methods:{
+      getTx(txhash){
+        axios.get('http://localhost:8080/transaction/getTransactionInfoByTxhash', {
+          params: {
+            txhash:txhash
+             }
+        })
+        .then(function (response) {
+          console.log(response);
+          app.showTx=response.data;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }
+  })
